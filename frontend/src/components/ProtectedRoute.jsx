385da1ba9b72302d70a7b9af0de53b40787ed2b7
Prathviesh <prathvieshnaik@gmail.com>
2026-06-20 01:30:51 +0530

    Fixed protectedRoute for auth

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,30 +1,9 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useNavigate, Navigate } from "react-router-dom";
+import {  Navigate } from "react-router-dom";
 import Spinner from "./Spinner";
-import { getCurrentUser } from "../services/authServices";
+import useAuth from "../hooks/useAuth";
 
 const ProtectedRoute = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      setLoading(true);
-      try {
-        const user = await getCurrentUser();
-
-        if (user) {
-          setIsAuthenticated(true);
-        }
-      } catch (error) {
-        setIsAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-    checkAuth();
-  }, []);
+  const {isAuthenticated, loading} = useAuth();
 
   if (loading) {
     return <Spinner />;
