5fd710f1b9e8f449f75d5a0009941856766d0f57
Prathviesh <prathvieshnaik@gmail.com>
2026-06-21 17:12:37 +0530

    Project complete

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,54 +1,6 @@
 import axios from "axios";
-const API_URL = "http://localhost:3000/api/auth";
-
-const loginUser = async (userData) => {
-  try {
-    const response = await axios.post(`${API_URL}/login`, userData, {
-      withCredentials: true,
-    });
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const logoutUser = async () => {
-  try {
-    const response = await axios.post(`${API_URL}/logout`, null, {
-      withCredentials: true,
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const registerUser = async (userData) => {
-  try {
-    const response = await axios.post(`${API_URL}/signup`, userData, {
-      withCredentials: true,a
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const getCurrentUser = async () => {
-  try {
-    const response = await axios.get(`${API_URL}/profile`, {
-      withCredentials: true,
-    });
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export { loginUser, registerUser, getCurrentUser, logoutUser };
-
+const API_URL =
+  "https://mern-auth-practice-nl3l.onrender.com/api/auth"
 
 const loginUser = async (userData) => {
   try {
@@ -97,3 +49,4 @@ const getCurrentUser = async () => {
 };
 
 export { loginUser, registerUser, getCurrentUser, logoutUser };
+
